ac80fcae8d29a235d121d36081b076c3c7d070e6
Bia Dev <biacalepso9@gmail.com>
2022-03-10 23:10:17 -0300

    Adiciona sistema de rotas, estilo global, assets e front da Home Page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,2 +1,14 @@
 import React from "react";
-import {BrowserRouter, Route} from "react-router-dom"
+import {BrowserRouter, Route} from "react-router-dom" //páginas para se locomover/navegação dentro do site.
+import Home from "./pages/Home"
+//import TeacherList from "./pages/TeacherList"
+//import TeacherForm from "./pages/TeacherForm"
+
+function Routes () {
+    return (<BrowserRouter>
+        <Route path="/" component={Home}/> {/*path / == home*/}
+        {/* <Route path="/teachers" component={TeacherList}/>
+        <Route path="/form" component={TeacherForm}/> */}
+    </BrowserRouter>)
+}
+export default Routes
